833b82c05e2299e6180298c7bd0cfbf2246c1364
YugandharRepo <yugandhar.g414@gmail.com>
2025-09-26 12:29:41 +0530

    ui responsive

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,9 @@ import Loader from "./Components/Loader/Loader";
 const Oakandsomke = lazy(() => import("./Pages/Oakandsomke/Ordernowsomke"));
 const Menu = lazy(() => import("./Pages/Oakandsomke/Menu"));
 const Shoopingcart = lazy(() => import("./Pages/Oakandsomke/Shoopingcart"));
+const Review = lazy(() => import("./Pages/Oakandsomke/Review"));
+const Pickupdeviler = lazy(() => import("./Pages/Oakandsomke/Pickupdeviler"));
+
 
 function App() {
   return (
@@ -44,6 +47,8 @@ function App() {
             <Route path="/" element={<Oakandsomke/>} />
             <Route path="/menu" element={<Menu/>} />
             <Route path="/shoopingcart" element={<Shoopingcart/>} />
+            <Route path="/review" element={<Review/>} />
+            <Route path="/pickupdeviler" element={<Pickupdeviler/>} />
           </Routes>
         </Suspense>
       </Router>
